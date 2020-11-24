b859505b094def23a5a7d78afb51924088e29b22
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-24 12:56:16 -0800

    adding styles to post and installing UI material

--- src/components/post/post.component.js
+++ src/components/post/post.component.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './post.styles.css';
 
 function Post() {
     return (
-        <div>
+        <div className='post_container'>
             <h3>Username</h3>
             <img
+                className='post_image'
                 src='https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png'
             />
-            <h4>username: caption</h4>
+            <h4 className='post_text'><strong>username:</strong> caption</h4>
         </div>
     )
 }
