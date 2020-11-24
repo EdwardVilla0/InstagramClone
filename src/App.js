b56f77e1c1c1701fe043e683521ea1ff4035f76a
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-24 12:24:35 -0800

    created the main header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className='app_header'>
+        <img
+          className='app_headerImage'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/150px-Instagram_logo.svg.png'
+          alt='instagram logo'
+        />
+      </div>
       <h1>hello</h1>
     </div>
   );
