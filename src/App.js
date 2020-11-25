d82e45c76cfcfd4e64200986fb041890445ea6e4
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-25 13:39:01 -0800

    setting use state in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './components/post/post.component';
 
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "clever",
+      caption: "it worked",
+      imageUrl: "https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png"
+    }
+  ]);
+
   return (
-    <div className="App">
+    <div className="app">
 
       <div className='app_header'>
+
+
+
         <img
           className='app_headerImage'
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/150px-Instagram_logo.svg.png'
@@ -15,11 +26,11 @@ function App() {
         />
       </div>
       <h1>hello</h1>
-      <Post
-        username='clever'
-        caption='it works'
-        imageUrl='https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png' />
-
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
     </div>
   );
 }
