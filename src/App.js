22fd3e09751e1e5910a7de50ad7cc74bc9ac580c
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-25 20:18:00 -0800

    added modal using material UI

--- src/App.js
+++ src/App.js
@@ -2,29 +2,63 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './components/post/post.component';
 import { db } from './firebase/firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "clever",
-      caption: "it worked",
-      imageUrl: "https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png"
-    }
-  ]);
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, [])
+
 
   return (
     <div className="app">
 
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>i am a modal</h2>
+        </div>
+      </Modal>
+
       <div className='app_header'>
-
-
 
         <img
           className='app_headerImage'
@@ -34,8 +68,8 @@ function App() {
       </div>
       <h1>hello</h1>
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
     </div>
