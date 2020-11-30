41c59e4728b970870e12d6ee98a34b0a1b939999
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-30 13:03:46 -0800

    added an upload file selector in order to upload posts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { auth, db } from './firebase/firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import ImageUpload from './components/image-upload/ImageUpload.component';
 
 
 
@@ -96,6 +97,8 @@ function App() {
 
   return (
     <div className="app">
+
+      <ImageUpload />
 
       <Modal
         open={open}
---
+++ src/components/image-upload/ImageUpload.component.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+
+function ImageUpload() {
+    const [image, setImage] = useState(null);
+    // const [progress, setProgress] = useState(0);
+    const [caption, setCaption] = useState('');
+
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0])
+        }
+    };
+
+    const handleUpload = () => {
+
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder='enter a caption...'
+                onChange={event => setCaption(event.target.value)}
+                value={caption} />
+
+            <input type="file" onChange={handleChange} />
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
+        </div>
+    )
+}
+
+export default ImageUpload;
